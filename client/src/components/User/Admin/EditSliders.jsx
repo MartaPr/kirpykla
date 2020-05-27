2aedaaee74eb5cffc7708affd68f6b977bec1372
Marta Praporščikaitė <marta.praporscikaite@gmail.com>
2020-05-27 23:11:13 +0300

    slider fix

--- client/src/components/User/Admin/EditSliders.jsx
+++ client/src/components/User/Admin/EditSliders.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 
 const EditSliders = (props) => {
-  const showSlides = () =>
-    props.slides.map((item) => {
-      return (
-        <div key={item._id} className="product">
-          <div className="product product--list-block">
-            <div className="product product--list">
-              <div className="slider-item">{item.title}</div>
-              <div className="slider-item">{item.description}</div>
-              <div className="slider-item">{item.publish}</div>
-            </div>
-            <div className="product slider-buttons">
-              <button
-                className="btn btn__btn-small btn--red"
-                onClick={() => props.deleteSlide(item._id)}
-              >
-                Trinti
-              </button>
-            </div>
+  const showSlides = () => {};
+  return props.slides.map((item) => {
+    return (
+      <div key={item._id} className="slider">
+        <div className="slider slider--list-block">
+          <div className="slider slider--list">
+            <div className="slider-item-title">{item.title}</div>
+            <p className="slider-item-description">{item.description}</p>
+          </div>
+          <div className="slider slider-buttons">
+            <button
+              className="btn btn__btn-small btn--red"
+              onClick={() => props.deleteSlide(item._id)}
+            >
+              Trinti
+            </button>
           </div>
         </div>
-      );
-    });
+      </div>
+    );
+  });
   return <div>{showSlides()}</div>;
 };
 
